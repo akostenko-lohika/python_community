d3dea72329140af3596e31c3974222e5cdbdc902
Andriy Kostenko <akostenko@lohika.com>
2019-10-22 06:31:03 +0000

    added addbook, getbook methods

--- grpc-ex/client.py
+++ grpc-ex/client.py
@@ -1,20 +1,59 @@
 from __future__ import print_function
 import logging
-
+import sys
 import grpc
 
 import library_pb2
 import library_pb2_grpc
 from google.protobuf.empty_pb2 import Empty
+from PyInquirer import prompt
 
 
-def run():
+def add_book():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = library_pb2_grpc.LibraryStub(channel)
+        questions = [
+            {'type': 'input', 'name': 'title', 'message': 'book title'},
+            {'type': 'input', 'name': 'author', 'message': 'book author'},
+            {'type': 'input', 'name': 'isbn', 'message': 'book isbn'},
+        ]
+        answers = prompt(questions)
+        response = stub.AddBook(library_pb2.BookRequest(
+            title=answers['title'],
+            author=answers['author'],
+            isbn=answers['isbn'])
+        )
+        print("Book:\n" + str(response))
+
+
+def get_book():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = library_pb2_grpc.LibraryStub(channel)
+        questions = [
+            {'type': 'input', 'name': 'isbn', 'message': 'book isbn'}
+        ]
+        answers = prompt(questions)
+        response = stub.GetBook(library_pb2.BookRequest(isbn=answers['isbn']))
+        if response.isbn == answers['isbn']:
+            print("Book:\n" + str(response))
+        else:
+            print(f"no book with ISBN: {answers['isbn']}")
+
+
+def list_books():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = library_pb2_grpc.LibraryStub(channel)
         response = stub.ListBooks(Empty())
-        print("Client received: " + response.books)
+        for item in response.books:
+            print("Book:\n" + str(item))
 
 
 if __name__ == '__main__':
     logging.basicConfig()
-    run()
+    if len(sys.argv) > 1:
+        if str(sys.argv[1]) == 'addbook':
+            add_book()
+        elif str(sys.argv[1]) == 'getbook':
+            get_book()
+    else:
+        list_books()
--- grpc-ex/servicer.py
+++ grpc-ex/servicer.py
@@ -1,30 +1,63 @@
 from concurrent import futures
 import grpc
 import logging
+import sqlite3
+
+import pandas as pd
 
 from library_pb2_grpc import LibraryServicer
 from library_pb2 import BookList, Book
+from google.protobuf.empty_pb2 import Empty
 
 import library_pb2_grpc
 
 
 class Servicer(LibraryServicer):
     def ListBooks(self, request, context):
-        return BookList(
-            books=[
-                Book(title="The Dark Tower: The Gunslinger",
-                     author="Stephen King", isbn="978-0-937986-50-9"),
-                Book(title="The Talisman", author="Stephen King", isbn="978-0-670-69199-9"),
-                Book(title="Neverwhere"),
-        ])
+        conn = sqlite3.connect('TestDB.db')
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+        c.execute('''SELECT * FROM books''')
+        results = c.fetchall()
+        return_books = [Book(title=res['title'], author=res['author'], isbn=res['isbn']) for res in results]
+        return BookList(books=return_books)
+
+    def GetBook(self, request, context):
+        conn = sqlite3.connect('TestDB.db')
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+        c.execute(f'SELECT * FROM books WHERE isbn=\'{request.isbn}\'')
+        result = c.fetchone()
+        return_book = Book(title=result['title'], author=result['author'], isbn=result['isbn']) if result else Empty()
+        return return_book
+
+    def AddBook(self, request, context):
+        conn = sqlite3.connect('TestDB.db')
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+        query = f'INSERT INTO books (title, author, isbn) VALUES (\'{request.title}\',\'{request.author}\',\'{request.isbn}\');'
+        c.execute(query)
+        conn.commit()
+        return Book(title=request.title, author=request.author, isbn=request.isbn)
+
 
 def serve():
+    create_test_db()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     library_pb2_grpc.add_LibraryServicer_to_server(Servicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     logging.info("Starting server on :50051")
     server.wait_for_termination()
+
+
+def create_test_db():
+    conn = sqlite3.connect('TestDB.db')
+    read_books = pd.read_csv(r'books_test_data/books.csv')
+    try:
+        read_books.to_sql('books', conn, if_exists='fail', index=False)
+    except ValueError:
+        pass
 
 
 if __name__ == '__main__':
